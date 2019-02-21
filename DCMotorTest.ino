1e8601f48a3f0097d98e81220bfcae8cf3e3988e
changji2069 <33982721+changji2069@users.noreply.github.com>
2019-02-21 10:59:28 +0000

    Update DCMotorTest.ino

--- DCMotorTest.ino
+++ DCMotorTest.ino
@@ -1,30 +1,28 @@
-#include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
+Adafruit_DCMotor *LMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *RMotor = AFMS.getMotor(2);
 
 void setup() {
   Serial.begin(9600);
   AFMS.begin(); 
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHzx
   
-  myMotor->setSpeed(150);
-  myMotor->run(FORWARD);
-  myMotor2->setSpeed(150);
-  myMotor2->run(FORWARD); 
 }
 
 void loop() {
-  uint8_t i;
-  
-  myMotor->run(FORWARD);
-  myMotor2->run(FORWARD);
-  for (i=255; i!=0; i--) {
-    myMotor->setSpeed(i);
-    myMotor2->setSpeed(i);
-    delay(10);
-  }
+  turn90(1000);
+}
+
+void turn90(int T) {
+  LMotor->run(FORWARD);
+  RMotor->run(BACKWARD);
+  LMotor->setSpeed(150);
+  RMotor->setSpeed(150);
+  delay(T);
+  LMotor->setSpeed(0);
+  RMotor->setSpeed(0);
+  exit(0);
 }
